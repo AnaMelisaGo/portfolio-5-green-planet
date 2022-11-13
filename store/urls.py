f45245195a688c84797533cfc7eb46865b30370b
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-13 18:33:40 +0000

    Add store detail page and add hover effect

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('all/', views.all_stores, name='all_stores'),
+    path('<store_id>', views.store_detail, name='store_detail')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Store
 
 
@@ -14,3 +14,16 @@ def all_stores(request):
         'stores': stores,
     }
     return render(request, 'stores/all_stores.html', context)
+
+
+def store_detail(request, store_id):
+    """
+    To view a single store and its detail
+    """
+    store = get_object_or_404(Store, pk=store_id)
+
+    context = {
+        'stores': 'active',
+        'store': store,
+    }
+    return render(request, 'stores/store_detail.html', context)
