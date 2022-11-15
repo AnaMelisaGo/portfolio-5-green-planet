dd319a13c840439ec738887713f31f952baa71a5
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-15 17:26:03 +0000

    Add content to bag.htm to view purchase

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,9 +1,23 @@
+from django.shortcuts import get_object_or_404
+from store.models import Store
+
+
 def bag_contents(request):
     """
     Context to be used for the entire website
     """
     bag_items = []
     grand_total = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        store = get_object_or_404(Store, pk=item_id)
+        grand_total += quantity * store.price
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'store': store,
+        })
 
     context = {
         'bag_items': bag_items,
