eab162bcf8c13c9b06e7b52b1763a1ea37f55054
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-16 11:54:55 +0000

    Add toast for the messages to give feedback to users

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+from store.models import Store
 
 
 def view_bag(request):
@@ -15,14 +18,19 @@ def add_item(request, item_id):
     """
     Add item to bag
     """
+    store = Store.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
-        messages.error(request, 'This item is in the bag')
+        messages.error(
+            request, 'This item is in the bag already.' +
+            ' Only one bag can be purchased. Sorry for the inconvinience!'
+        )
     else:
         bag[item_id] = quantity
+        messages.success(request, f'You added {store.store_name} to the bag')
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
