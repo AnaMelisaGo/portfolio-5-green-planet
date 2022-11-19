3f17d09335c33af5057eba9fbed4d4637ef55863
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-19 20:44:33 +0000

    Add stripe basic functionality to make payments

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,7 @@
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-let client_secret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
+// Initialize stripe and add style from boutique ado walkthrough
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 let style = {
     base: {
@@ -19,3 +20,49 @@ let style = {
 };
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// validation of error from boutique ado walkthrough
+card.addEventListener('change', function (event) {
+    let errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle a card payment
+let form = document.getElementById('transaction-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -1,23 +1,43 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import TransactionForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
     """
     To render the checkout form
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, 'No contents in the bag.')
         return redirect(reverse('all_stores'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+    
     transaction_form = TransactionForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Public key is missing. \
+            Are you sure you set it in your environment?')
     template = 'checkout/checkout.html'
     context = {
         'transaction_form': transaction_form,
-        'stripe_public_key': 'pk_test_51LSarLAI6gj8NSENMJNRtzO0qqUAjA0PuMZpFne5zEPtdq51Et7CbI8FgklAdFoLVJ2xxPqwSTWpBCD5HOeMaqFa000xMo8U8h',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)
