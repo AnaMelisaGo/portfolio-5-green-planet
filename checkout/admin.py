ec3e7a0380d5f6ac9d29502664d48a8361455856
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-19 06:58:48 +0000

    Add forms.py and admin for the checkout app

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Transaction, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('order_total',)
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('transaction_number', 'date',
+                       'grand_total',)
+
+    fields = ('transaction_number', 'date', 'full_name',
+              'email', 'phone_number', 'grand_total',)
+
+    list_display = ('transaction_number', 'date', 'full_name',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Transaction, TransactionAdmin)
+
+
