e25d4798893a7eafb663ffdd3fe6f4868884c78d
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-22 06:43:43 +0000

    Add profile view and url and add edit profile view and url

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def user_profile(request):
     """
     Function to show the user profile
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
+    return render(request, template, context)
+
+
+def edit_profile(request):
+    """
+    Function to edit the user profile
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+    transactions = profile.account.all()
+    template = 'profiles/edit_profile.html'
+    context = {
+        'profile': profile,
+        'form': form,
+        'transactions': transactions,
+    }
     return render(request, template, context)
