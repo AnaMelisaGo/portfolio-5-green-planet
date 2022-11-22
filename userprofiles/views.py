41809c9b5156894ed12c5614b9e401d2707c5c7d
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-22 09:27:05 +0000

    Edit toast success and userprofiles view when editing profile

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -20,11 +22,22 @@ def edit_profile(request):
     Function to edit the user profile
     """
     profile = get_object_or_404(UserProfile, user=request.user)
-    form = UserProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=profile)
     template = 'profiles/edit_profile.html'
     context = {
         'profile': profile,
         'form': form,
+        'on_profile_page': True,
     }
     return render(request, template, context)
 
@@ -34,7 +47,7 @@ def view_history(request):
     Function to view user's transactions history
     """
     profile = get_object_or_404(UserProfile, user=request.user)
-    
+
     transactions = profile.account.all()
     template = 'profiles/view_history.html'
     context = {
