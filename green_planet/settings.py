ae30d353e9f426acc2f289c206779d6b91dfb06b
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-23 11:18:17 +0000

    Add cache control

--- green_planet/settings.py
+++ green_planet/settings.py
@@ -185,6 +185,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Connect to AWS S3
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'portfolio-5-green-planet'
     AWS_S3_REGION_NAME = 'eu-west-1'
