b63ac48e26663f51a31cd869927bb2c5607722f1
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-23 16:37:44 +0000

    Add user email in edit profile form

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'email')
 
     def __init__(self, *args, **kwargs):
         """
@@ -17,6 +17,7 @@ class UserForm(forms.ModelForm):
         placeholders = {
             'first_name': 'First Name',
             'last_name': 'Last Name',
+            'email': 'Email Address'
         }
 
         self.fields['first_name'].widget.attrs['autofocus'] = True
