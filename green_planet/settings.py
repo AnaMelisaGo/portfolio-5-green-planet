30e29f34a60b5060f6d7fed9843367a08c7fa362
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-23 10:13:00 +0000

    Change secret key and set debug

--- green_planet/settings.py
+++ green_planet/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a8uqde2r-46k%gj@(yz$9^h^$ivn^-o*c=g%oubz*rtgp+x3=#'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['portfolio-5-green-planet.herokuapp.com', 'localhost']
 
