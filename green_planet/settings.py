ac3485460a6499b6b1e3d252235070f8e7884494
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-25 19:49:12 +0000

    Make changes to deploy to heroku

--- green_planet/settings.py
+++ green_planet/settings.py
@@ -193,7 +193,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket config
-    AWS_STORAGE_BUCKET_NAME = 'portfolio-5-green-planet'
+    AWS_STORAGE_BUCKET_NAME = 'project-5-green-planet'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
