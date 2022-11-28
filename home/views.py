eddd01c8541e32a648a3e3a407042b3f43059535
AnaMelisaGo <melisago.rockn@gmail.com>
2022-11-28 06:56:43 +0000

    Add settings for iamresponsive

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 
+@xframe_options_exempt
 def index(request):
     """
     To render home page
